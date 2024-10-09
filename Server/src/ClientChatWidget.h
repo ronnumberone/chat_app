7d6fdaa7c9348fbcbf6d6c8d7a1d74358e556cba
Hoang Nguyen <nguyengiahoang0104@gmail.com>
2024-10-10 00:30:38 +0700

    message transfer between client and server

---
+++ Server/src/ClientChatWidget.h
@@ -0,0 +1,28 @@
+#ifndef CLIENTCHATWIDGET_H
+#define CLIENTCHATWIDGET_H
+#include <QTcpSocket>
+#include <QWidget>
+
+namespace Ui {
+class ClientChatWidget;
+}
+
+class ClientChatWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ClientChatWidget(QTcpSocket *client, QWidget *parent = nullptr);
+    ~ClientChatWidget();
+
+private slots:
+    void dataReceived();
+    void clientDisconnected();
+    void on_btnSend_clicked();
+
+private:
+    Ui::ClientChatWidget *ui;
+    QTcpSocket *_client;
+};
+
+#endif // CLIENTCHATWIDGET_H
